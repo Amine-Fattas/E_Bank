40eee785d27cd1f4c1883823f6ad929a07a54890
ikram <ikram@hotmail.com>
2020-06-11 16:01:10 +0100

    back agent apres echec de tracabilité

--- agent_back/src/main/java/com/ensa/e_banking/dao/OperationRepository.java
+++ agent_back/src/main/java/com/ensa/e_banking/dao/OperationRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.ensa.e_banking.entities.Compte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -23,6 +24,13 @@ public interface OperationRepository extends JpaRepository<Operation, Long>{
 
 	@Query("select o from Operation o  where o.compteSource.rib like :x or o.typeOperation like :x or  o.compteDestination.rib like :x ")
 	public List<Operation> chercherOperation(@Param("x") String mc);
+
+	@Modifying
+	@Query(value ="delete FROM operation where  rib_source=:x or rib_destination=:x",nativeQuery=true)
+	int deleteOperationByCompte(@Param("x") String rib);
+
+
+
 
 
 }
